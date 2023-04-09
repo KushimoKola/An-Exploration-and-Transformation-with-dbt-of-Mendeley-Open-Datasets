61fe22853efe671eeb7251f3f000f254c0741e5c
KushimoKola <kushimo.kolawole@gmail.com>
2023-04-08 23:35:26 -0400

    [UPDATE] updated `staging` model
    Macro not working, so, had to result to handling each staging model indidually.

--- Warehouse_snowflake/models/staging/stg_dblp.sql
+++ Warehouse_snowflake/models/staging/stg_dblp.sql
@@ -1,10 +1,35 @@
-with source_model as (
-    {{ snowflake_flatten_json(
-        stg_table_name = ref('source_dblp_data'),
-        json_column = 'raw_data'
-    ) }} 
-),
-final as (
-    select * from source_model
+WITH flattened_dblp_data AS (
+SELECT
+    raw_data:"_id"."$oid" AS id,
+    raw_data:"_key" AS key,
+    raw_data:"author" AS author,
+    --author.value AS author,
+    --raw_data:"author"[0] AS author1,
+    --raw_data:"author"[1] AS author2,
+    --raw_data:"author"[2] AS author3,
+    raw_data:"title" AS title,
+    raw_data:"booktitle" AS book_title,
+    raw_data:"journal" AS journal,
+    raw_data:"ee" AS ee,
+    raw_data:"url" AS url,
+    raw_data:"mdate" AS mdate,
+    raw_data:"month" AS month,
+    raw_data:"year" AS year,
+    raw_data:"number" AS number,
+    raw_data:"pages" AS pages,
+    raw_data:"type" AS type,
+    raw_data:"volume" AS volume,
+    raw_data:"editor" AS editor,
+    raw_data:"school" AS school,
+    raw_data:"crossref" AS crossref,
+    raw_data:"publisher" AS publisher,
+    raw_data:"series" AS series,
+    raw_data:"cdrom" AS cdrom,
+    raw_data:"note" AS note,
+    raw_data:"isbn" AS isbn,
+    raw_data:"cite" AS citations,
+    raw_data:"publtype" AS publication_type
+FROM
+    analytics_dbt.source_data.source_dblp_data
 )
-select * from final;
+SELECT * from  flattened_dblp_data;
--- Warehouse_snowflake/models/staging/stg_nba.sql
+++ Warehouse_snowflake/models/staging/stg_nba.sql
@@ -1,10 +1,36 @@
-with source_model as (
-    {{ snowflake_flatten_json(
-        stg_table_name = ref('source_nba_games'),
-        json_column = 'raw_data'
-    ) }} 
-),
-final as (
-    select * from source_model
+WITH flattened_nba_data AS (
+SELECT
+    raw_data:"_id"."$oid" AS id,
+    raw_data:"date"."$date" AS date,
+    team.value:abbreviation AS abbreviation,
+    team.value:city AS city,
+    team.value:home AS is_home_team,
+    team.value:score AS team_score,
+    team.value:won AS team_won,
+    team.value:name AS name,
+    player.value:ast AS ast,
+    player.value:blk AS blk,
+    player.value:drb AS drb,
+    player.value:fg AS fg,
+    player.value:fg3 AS fg3,
+    player.value:fg3_pct AS fg3_pct,
+    player.value:fg3a AS fg3a,
+    player.value:fg_pct AS fg_pct,
+    player.value:fga AS fga,
+    player.value:ft AS ft,
+    player.value:ft_pct AS ft_pct,
+    player.value:fta AS fta,
+    player.value:mp AS mp,
+    player.value:orb AS orb,
+    player.value:pf AS pf,
+    player.value:player AS player,
+    player.value:pts AS pts,
+    player.value:stl AS stl,
+    player.value:tov AS tov,
+    player.value:trb AS trb
+FROM
+    {{ ref('source_nba_games') }},
+    LATERAL FLATTEN(raw_data:teams) team,
+    LATERAL FLATTEN(team.value:players) player
 )
-select * from final;
+SELECT * from  flattened_nba_data;
